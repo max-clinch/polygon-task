20ae6fd18b3af6eed832b073e18a284cdd833446
max-clinch <74224236+max-clinch@users.noreply.github.com>
2022-11-23 21:34:23 +0100

    initial

---
+++ truffle-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 3000,
+      network_id: "*" // Match any network id
+    }
+  }
+};
+module.exports = {
+  
+  compilers: {
+    solc: {
+      version: "0.8.17",
+    },
+  },
+  
+};
